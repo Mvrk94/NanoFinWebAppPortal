c2fc8a69e5ebce7f822afa6c2cbcf86f78ce63b4
Tshigabel <Tshigabel@outlook.com>
2016-08-10 20:50:21 +0200

    reports front end complete

--- NanoFinWebApp/src/NanoFinWebApp/scripts/components/InsuranceManager/dashboard.js
+++ NanoFinWebApp/src/NanoFinWebApp/scripts/components/InsuranceManager/dashboard.js
@@ -3,95 +3,108 @@
     {
         var map;
 
-
-        angular.element(document).ready(function ()
+        var Vmaps;
+       
+        var BestProductSuccess = function (responce)
         {
-           
-            
-        });
+            var data = responce.data;
+            var currentBest = data[0];
+            var currentWorst = data[0];
+            var counter = 0;
+            for (var p in data)
+            {
+                if (parseInt(responce.data[counter].overallUsage) > parseInt(currentBest.overallUsage))
+                {
 
-
-        var errorCallBack = function (response)
-        {
-
-            $scope.insuranceProductList = response.data;
+                   currentBest = data[counter];
+                }
+                else if (parseInt(responce.data[counter].overallUsage) < parseInt(currentWorst.overallUsage))
+                {
+                    
+                    currentWorst = data[counter];
+                }
+                counter++;
+            }
+            $scope.bestProduct = currentBest;
+            $scope.worstProduct = currentWorst;
         };
 
-        var successCallBack = function (response)
+        var getSales = function (response)
         {
+            $scope.numOverallSales = response.data.numOverallSales;
+            $scope.mySales = response.data.mySales;
+            $scope.MarketShare = ($scope.mySales * 100) / $scope.numOverallSales;
+        };
+
+        var getMemeber = function (responce) {
+            $scope.memebers = responce.data;
+        };
+
+        var successCallBack = function (response) {
             $scope.insuranceProductList = response.data;
             var mapMarkers = [];
+            var details = response.data;
+            var counter = 0;
+            for (var p in details) {
+                var value = 'vouchers' + String(response.data[counter].voucherSent) + 'sent at' + response.data[counter].address.split(':')[2];
+                mapMarkers.push(
+                    {
+                        latLng: [parseFloat(response.data[counter].address.split(':')[0]), parseFloat(response.data[counter].address.split(':')[1])],
+                        name: value
+                    });
+                counter++;
+            }
+
+            var setAreaRatings = function ()
+            {
+               
 
 
-                
-      mapMarkers.push({ latLng: [43.73, 7.41], name: 'Monaco' });
-      mapMarkers.push({ latLng: [-0.52, 166.93], name: 'Nauru' });
-      mapMarkers.push({ latLng: [-8.51, 179.21], name: 'Tuvalu' });
-      mapMarkers.push({ latLng: [43.93, 12.46], name: 'San Marino' });
-      mapMarkers.push({ latLng: [47.14, 9.52], name: 'Liechtenstein' });
-      mapMarkers.push({ latLng: [7.11, 171.06], name: 'Marshall Islands' });
-      mapMarkers.push({ latLng: [17.3, -62.73], name: 'Saint Kitts and Nevis' });
-      mapMarkers.push({ latLng: [3.2, 73.22], name: 'Maldives' });
-      mapMarkers.push({ latLng: [35.88, 14.5], name: 'Malta' });
-      mapMarkers.push({ latLng: [12.05, -61.75], name: 'Grenada' });
-      mapMarkers.push({ latLng: [13.16, -61.23], name: 'Saint Vincent and the Grenadines' });
-      mapMarkers.push({ latLng: [13.16, -59.55], name: 'Barbados' });
-      mapMarkers.push({ latLng: [17.11, -61.85], name: 'Antigua and Barbuda' });
-      mapMarkers.push({ latLng: [-4.61, 55.45], name: 'Seychelles' });
-      mapMarkers.push({ latLng: [42.5, 1.51], name: 'Andorra' });
-      mapMarkers.push({ latLng: [14.01, -60.98], name: 'Saint Lucia' });
-      mapMarkers.push({ latLng: [6.91, 158.18], name: 'Federated States of Micronesia' });
-      mapMarkers.push({ latLng: [1.3, 103.8], name: 'Singapore' });
-      mapMarkers.push({ latLng: [1.46, 173.03], name: 'Kiribati' });
-      mapMarkers.push({ latLng: [-21.13, -175.2], name: 'Tonga' });
-      mapMarkers.push({ latLng: [15.3, -61.38], name: 'Dominica' });
-      mapMarkers.push({ latLng: [-20.2, 57.5], name: 'Mauritius' });
-      mapMarkers.push({ latLng: [26.02, 50.55], name: 'Bahrain' });
-      mapMarkers.push({ latLng: [0.33, 6.73], name: 'São Tomé and Príncipe' });
-            
-
-            alert(response.data[0].address);
-            //for (var i = 0, l = response.data.length; i < l; i++)
-            //    mapMarkers.push({ latLng: "[" +response.data[i].address + "]", name: 'Vatican City'  });
+            };
 
 
 
-            //var markers = [];
-            //markers.push({ latLng: mapMarkers });
-
-            $('#world-map-markers').vectorMap({
+            Vmaps = $('#world-map-markers').vectorMap(
+            {
                 map: 'world_mill_en',
                 normalizeFunction: 'polynomial',
                 hoverOpacity: 0.7,
                 hoverColor: false,
+                zoomMin: 1,
                 backgroundColor: 'transparent',
-                regionStyle: {
-                    initial: {
+                regionStyle:
+                {
+                    initial:
+                    {
                         fill: 'rgba(210, 214, 222, 1)',
                         "fill-opacity": 1,
                         stroke: 'none',
                         "stroke-width": 0,
                         "stroke-opacity": 1
                     },
-                    hover: {
+                    hover:
+                    {
                         "fill-opacity": 0.7,
                         cursor: 'pointer'
                     },
-                    selected: {
+                    selected:
+                    {
                         fill: 'yellow'
                     },
                     selectedHover: {}
                 },
-                markerStyle: {
-                    initial: {
+                markerStyle:
+                {
+                    initial:
+                    {
                         fill: '#00a65a',
                         stroke: '#111'
                     }
                 },
                 markers: mapMarkers
             });
-            //mapMarkers.length = 0;
 
+            //var tt = $('#world-map-markers').getMapObject();
             //for (var i = 0, l = response.data.length; i < l; i++)
             //{
             //    mapMarkers.push({latLng: response.data[i].address });
@@ -101,11 +114,52 @@
 
         };
 
+        var getUnprocessedApplications = function (responce) {
+            $scope.unprocessedApplications = responce.data;
+        };
+
+        var errorCallBack = function (response) {
+            $scope.insuranceProductList = response.data;
+        };
+
+      
+
         $http(
-        {
-            method: 'GET',
-            url: 'http://nanofinapi.azurewebsites.net/api/Reports/getBestReseller'
-        })
-        .then(successCallBack, errorCallBack);
+       {
+           method: 'GET',
+           url: 'http://nanofinapi.azurewebsites.net/api/Reports/getBestSellingProduct'
+       }).then(BestProductSuccess,errorCallBack);
+        
+        $http(
+     {
+         method: 'GET',
+         url: 'http://nanofinapi.azurewebsites.net/api/Reports/getBestReseller'
+     })
+     .then(successCallBack, errorCallBack);
+
+
+        $http(
+       {
+           method: 'GET',
+           url: 'http://nanofinapi.azurewebsites.net/api/Reports/getMembers?ProviderID=11'
+       }).then(getMemeber, errorCallBack);
+
+        
+       $http(
+       {
+           method: 'GET',
+           url: 'http://nanofinapi.azurewebsites.net/api/Reports/getOverallPurchases?Provider_ID=11'
+       }).then(getSales, errorCallBack);
+
+
+       $http(
+      {
+          method: 'GET',
+          url: 'http://nanofinapi.azurewebsites.net/api/Reports/getNumberOfUnprocessedApplications?ProviderID=11'
+      }).then(getUnprocessedApplications, errorCallBack);
+
+       
+
+       
 
     }]);
