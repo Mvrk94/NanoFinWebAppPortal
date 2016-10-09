2edf94a0e73cf054e5a2aca88fd204da9fd52741
Tshigabel <Tshigabel@outlook.com>
2016-10-09 14:18:20 +0200

    consumer profiles risk reports added

--- PORTAL/src/NanoFinWebApp/scripts/components/InsuranceManager/UnrealEngine.js
+++ PORTAL/src/NanoFinWebApp/scripts/components/InsuranceManager/UnrealEngine.js
@@ -11,202 +11,9 @@
     var isfilterinhAgeGroup = { isFilter: false, value: "All" , type : "ageGroup" ,possibleValues: [2,12,22,32] };
     var isfilteringRiskCat = { isFilter: false, value: "All", type: "RiskCategory", possibleValues: [1,2,3,4] };
 
-   
-
-
-    function init()
-    {
-        originalData = JSON.parse(httpRequest("POST", "http://nanofinapifinal.azurewebsites.net/api/ProcessInsuranceApplications/getUnprocessedApplications"));
-        
-        for(var  i = 0 ; i < originalData.length ; i++)
-        {
-            filteredData.push(originalData[i]);
-        }
-        document.getElementById("btnRefresh").onclick = function ()
-        {
-            isfilteringGender.value = document.getElementById("cmbGender").value;
-            isfilteringEmploymentStatus.value = document.getElementById("cmbEmployment").value;
-            isfilteringMaritalStatus.value = document.getElementById("cmbMarital").value;
-            isfilterinhAgeGroup.value = document.getElementById("cmbAgeGroup").value;
-            isfilteringRiskCat.value = document.getElementById("cmbRiskCat").value;
-            FilterAllData();
-        };
-    }
-
-    function FilterAllData()
-    {
-        isfilteringGender.isFilter = (isfilteringGender.value === "All") ? false : true;
-        isfilteringEmploymentStatus.isFilter = (isfilteringEmploymentStatus.value === "All") ? false : true;
-        isfilteringMaritalStatus.isFilter =  (isfilteringMaritalStatus.value === "All") ? false : true;
-        isfilterinhAgeGroup.isFilter = (isfilterinhAgeGroup.value === "All") ? false : true;
-        isfilteringRiskCat.isFilter = (isfilteringRiskCat.value === "All") ? false : true;
-    
-        var filterArr = [ isfilteringGender, isfilteringMaritalStatus, isfilteringEmploymentStatus, isfilterinhAgeGroup, isfilteringRiskCat ];
-        filteredData = [];
-        for (var c = 0 ; c < originalData.length ; c++) 
-            filteredData.push(originalData[c]);
-
-        for(var  i  = 0 ; i <  filterArr.length ; i++)
-        {
-            if(filterArr[i].isFilter)
-            {
-                alert("selected " + filterArr[i].type);
-                filteredData = FilterGroup(filteredData, filterArr[i].type, filterArr[i].value);
-            }
-        }
-        drawConsumerReport(filterArr);
-    }
-
-    $(document).ready(function () {
-        init();
-        drawConsumerReport();
-    });
-
     /*
-        CONSUMER REPORTS
-    */
-    function drawConsumerReport(filterArr)
-    {
-        drawConsumerGenderReports();
-        drawConsumerMaritalStatusReports();
-        drawConsumerEmployedReports();
-        drawConsumerAgeGroupReports();
-        drawConsumerRiskCatReports();
-    }
-    function drawConsumerGenderReports()
-    {
-        clearGraphCanvas("ConsumerReportGender");
-        if (isfilteringGender.isFilter) return;
-        drawBarGraph("ConsumerReportGender", "ConsumerReportMaritalBarChart", "Gender ", "gender", isfilteringGender.possibleValues, isfilteringGender.possibleValues);
-    }
-    function drawConsumerMaritalStatusReports()
-    {
-        clearGraphCanvas("ConsumerReportMarital");
-        if (isfilteringMaritalStatus.isFilter) return;
-        drawBarGraph("ConsumerReportMarital", "consumerMaritalBarChart", "Marital Status", "maritalStatus", isfilteringMaritalStatus.possibleValues, isfilteringMaritalStatus.possibleValues);
-    }
-    function drawConsumerEmployedReports()
-    {
-        clearGraphCanvas("ConsumerReportEmployment");
-        if (isfilteringEmploymentStatus.isFilter) return;
-        drawBarGraph("ConsumerReportEmployment", "consumerEmploymentBarChart", "Employment Status", "employmentStatus", isfilteringEmploymentStatus.possibleValues, isfilteringEmploymentStatus.possibleValues);
-    }
-    function drawConsumerAgeGroupReports()
-    {
-        clearGraphCanvas("ConsumerReportAge");
-        if (isfilterinhAgeGroup.isFilter) return;
-        var labels = ["18-25", "26-30", "31-39", "40-60"];
-        drawBarGraph("ConsumerReportAge", "ConsumerReportAgeBarChart", "Age Group", "ageGroup", isfilterinhAgeGroup.possibleValues, labels);
-    }
-    function drawConsumerRiskCatReports()
-    {
-        clearGraphCanvas("RiskCatReportMarital");
-        if (isfilteringRiskCat.isFilter) return;
-        var labels = ["Low", "Moderate", "High", "Very-High"];
-        drawBarGraph("RiskCatReportMarital", "RiskCatReportMaritalBarChart", "Risk Category", "RiskCategory", isfilteringRiskCat.possibleValues, labels);
-    }
-    /*
-        RISK REPORTS
-    */
-    
-
-
-    /*
-        UNITILITY LIBRARY
-    */
-
-    function AddGraphHtml(chartTitle, chartID) {
-        var html = "";
-        html += "<div class='box box-primary'>";
-        html += "<div class='box-header with-border'>";
-        html += "<h3 class='box-title'>" + chartTitle + "</h3>";
-        html += "<div class='box-tools pull-right'>";
-        html += "<button type='button' class='btn btn-box-tool' data-widget='collapse'><i class='fa fa-minus'></i></button>";
-        html += "</div>";
-        html += "</div>";
-        html += "<div class='box-body'>";
-        html += "<div class='chart'>";
-        //Key
-        html += "<canvas id='" + chartID + "' style='height:250px'></canvas>";
-        html += "</div>";
-        html += "</div>";
-        html += "<!-- /.box-body -->";
-        html += "</div>";
-
-        return html;
-    }
-
-    function FilterGroup(filtered, variableType, request) {
-        var temp = [];
-        var counter = 0;
-
-        for (var i = 0 ; i < filtered.length ; i++) {
-            if (read_prop(filtered[i], variableType) == request)
-                temp.push(filtered[i]);
-        }
-        return temp;
-    }
-
-    function countData(filtered, variableType, request) {
-        var count = 0;
-        for (var i = 0 ; i < filtered.length ; i++) {
-            if (read_prop(filtered[i], variableType) == request)
-                count++;
-        }
-        return count;
-    }
-
-    function httpRequest(methode, theUrl)
-    {
-        var xmlHttp = new XMLHttpRequest();
-        xmlHttp.open(methode, theUrl, false); // false for synchronous request
-        xmlHttp.send(null);
-        return xmlHttp.responseText;
-    }
-
-    function read_prop(obj, prop) {
-        return obj[prop];
-    }
-
-    function  clearGraphCanvas(elementID)
-    {
-        document.getElementById(elementID).innerHTML = "";
-        document.getElementById(elementID).className = "";
-    }
-
-    function drawBarGraph(elementID, graphID, title, type, possibleValues, labelName) {
-        document.getElementById(elementID).innerHTML = AddGraphHtml(title, graphID);
-        document.getElementById(elementID).className = "col col-md-4";
-        var dataset = [];
-        var count = 0;
-        for (var i = 0 ; i < possibleValues.length ; i++) {
-            dataset.push(countData(filteredData, type, possibleValues[i]));
-        }
-
-        var barChartCanvas = $("#" + graphID).get(0).getContext("2d");
-        var barChart = new Chart(barChartCanvas);
-        var barChartData = {
-            labels: labelName,
-            datasets: [
-              {
-                  label: labelName,
-                  fillColor: "rgba(60,141,188,0.9)",
-                  strokeColor: "rgba(60,141,188,0.8)",
-                  pointColor: "#3b8bba",
-                  pointStrokeColor: "rgba(60,141,188,1)",
-                  pointHighlightFill: "#fff",
-                  pointHighlightStroke: "rgba(60,141,188,1)",
-                  data: dataset
-              }
-            ]
-        };
-        barChartOptions.datasetFill = false;
-        barChart.Bar(barChartData, barChartOptions);
-    }
-
-    /*
-        CHART-JS GRAPH OPTIONS
-    */
+     CHART-JS GRAPH OPTIONS
+ */
     var areaChartOptions = {
         //Boolean - If we should show the scale at all
         showScale: true,
@@ -296,4 +103,273 @@
         responsive: true,
         maintainAspectRatio: true
     };
+
+
+    function init()
+    {
+        originalData = JSON.parse(httpRequest("POST", "http://nanofinapifinal.azurewebsites.net/api/ProcessInsuranceApplications/getUnprocessedApplications"));
+        
+        for(var  i = 0 ; i < originalData.length ; i++)
+        {
+            filteredData.push(originalData[i]);
+        }
+        document.getElementById("btnRefresh").onclick = function ()
+        {
+            isfilteringGender.value = document.getElementById("cmbGender").value;
+            isfilteringEmploymentStatus.value = document.getElementById("cmbEmployment").value;
+            isfilteringMaritalStatus.value = document.getElementById("cmbMarital").value;
+            isfilterinhAgeGroup.value = document.getElementById("cmbAgeGroup").value;
+            isfilteringRiskCat.value = document.getElementById("cmbRiskCat").value;
+            FilterAllData();
+        };
+    }
+
+    function FilterAllData()
+    {
+        isfilteringGender.isFilter = (isfilteringGender.value === "All") ? false : true;
+        isfilteringEmploymentStatus.isFilter = (isfilteringEmploymentStatus.value === "All") ? false : true;
+        isfilteringMaritalStatus.isFilter =  (isfilteringMaritalStatus.value === "All") ? false : true;
+        isfilterinhAgeGroup.isFilter = (isfilterinhAgeGroup.value === "All") ? false : true;
+        isfilteringRiskCat.isFilter = (isfilteringRiskCat.value === "All") ? false : true;
+    
+        var filterArr = [ isfilteringGender, isfilteringMaritalStatus, isfilteringEmploymentStatus, isfilterinhAgeGroup, isfilteringRiskCat ];
+        filteredData = [];
+        for (var c = 0 ; c < originalData.length ; c++) 
+            filteredData.push(originalData[c]);
+
+        for(var  i  = 0 ; i <  filterArr.length ; i++)
+        {
+            if(filterArr[i].isFilter)
+            {
+                filteredData = FilterGroup(filteredData, filterArr[i].type, filterArr[i].value);
+            }
+        }
+        drawConsumerReport(filterArr);
+        drawRiskReports();
+    }
+
+    $(document).ready(function () {
+        init();
+        drawConsumerReport();
+    });
+
+    /*
+        CONSUMER REPORTS
+    */
+    function drawConsumerReport(filterArr)
+    {
+        drawConsumerGenderReports();
+        drawConsumerMaritalStatusReports();
+        drawConsumerEmployedReports();
+        drawConsumerAgeGroupReports();
+        drawConsumerRiskCatReports();
+    }
+    function drawConsumerGenderReports()
+    {
+        clearGraphCanvas("ConsumerReportGender");
+        if (isfilteringGender.isFilter) return;
+        drawBarGraph("ConsumerReportGender", "ConsumerReportMaritalBarChart", "Gender ", "gender", isfilteringGender.possibleValues, isfilteringGender.possibleValues);
+    }
+    function drawConsumerMaritalStatusReports()
+    {
+        clearGraphCanvas("ConsumerReportMarital");
+        if (isfilteringMaritalStatus.isFilter) return;
+        drawBarGraph("ConsumerReportMarital", "consumerMaritalBarChart", "Marital Status", "maritalStatus", isfilteringMaritalStatus.possibleValues, isfilteringMaritalStatus.possibleValues);
+    }
+    function drawConsumerEmployedReports()
+    {
+        clearGraphCanvas("ConsumerReportEmployment");
+        if (isfilteringEmploymentStatus.isFilter) return;
+        drawBarGraph("ConsumerReportEmployment", "consumerEmploymentBarChart", "Employment Status", "employmentStatus", isfilteringEmploymentStatus.possibleValues, isfilteringEmploymentStatus.possibleValues);
+    }
+    function drawConsumerAgeGroupReports()
+    {
+        clearGraphCanvas("ConsumerReportAge");
+        if (isfilterinhAgeGroup.isFilter) return;
+        var labels = ["18-25", "26-30", "31-39", "40-60"];
+        drawBarGraph("ConsumerReportAge", "ConsumerReportAgeBarChart", "Age Group", "ageGroup", isfilterinhAgeGroup.possibleValues, labels);
+    }
+    function drawConsumerRiskCatReports()
+    {
+        clearGraphCanvas("RiskCatReportMarital");
+        if (isfilteringRiskCat.isFilter) return;
+        var labels = ["Low", "Moderate", "High", "Very-High"];
+        drawBarGraph("RiskCatReportMarital", "RiskCatReportMaritalBarChart", "Risk Category", "RiskCategory", isfilteringRiskCat.possibleValues, labels);
+    }
+    /*
+        RISK REPORTS
+    */
+//<div id="RiskReportGender"></div>
+//<div id="RiskReportEmployment"></div>
+//<div id="RiskReportAge"></div>
+//<div id="MaritalReportRisk"></div>
+    function drawRiskReports()
+    {
+        drawRiskGenderReports();
+        drawRiskMaritalReports();
+        drawRiskEmployedReports();
+        drawRiskAgeGroupReports();
+    }
+
+    function drawRiskGenderReports()
+    {
+        clearGraphCanvas("RiskReportGender");
+        if (isfilteringGender.isFilter) return;
+        drawRiskBarGraph("RiskReportGender", "RiskReportGenderBarChart", "Gender Claim Rate", "gender", isfilteringGender.possibleValues, "claimRate", isfilteringGender.possibleValues);
+    }
+    function drawRiskAgeGroupReports()
+    {
+        clearGraphCanvas("RiskReportAge");
+        if (isfilterinhAgeGroup.isFilter) return;
+        drawRiskBarGraph("RiskReportAge", "RiskReportMaritalBarChart", "Age Group Claim Rate", "ageGroup", isfilterinhAgeGroup.possibleValues, "claimRate", isfilterinhAgeGroup.possibleValues);
+    }
+    function drawRiskMaritalReports()
+    {
+        clearGraphCanvas("RiskReportMarital");
+        if (isfilteringMaritalStatus.isFilter) return;
+        drawRiskBarGraph("RiskReportMarital", "MaritalReportRiskBarChart", "Marital Claim Rate", "maritalStatus", isfilteringMaritalStatus.possibleValues, "claimRate", isfilteringMaritalStatus.possibleValues);
+    }
+    function drawRiskEmployedReports()
+    {
+        clearGraphCanvas("RiskReportEmployment");
+        if (isfilteringEmploymentStatus.isFilter) return;
+        drawRiskBarGraph("RiskReportEmployment", "RiskReportEmploymentBarChart", "Employment Status Claim Rate", "employmentStatus", isfilteringEmploymentStatus.possibleValues, "claimRate", isfilteringEmploymentStatus.possibleValues);
+    }
+    
+    /*
+        UNITILITY LIBRARY
+    */
+
+    function AddGraphHtml(chartTitle, chartID,color) {
+        var html = "";
+        html += "<div class='box box-"+color+"'>";
+        html += "<div class='box-header with-border'>";
+        html += "<h3 class='box-title'>" + chartTitle + "</h3>";
+        html += "<div class='box-tools pull-right'>";
+        html += "<button type='button' class='btn btn-box-tool' data-widget='collapse'><i class='fa fa-minus'></i></button>";
+        html += "</div>";
+        html += "</div>";
+        html += "<div class='box-body'>";
+        html += "<div class='chart'>";
+        //Key
+        html += "<canvas id='" + chartID + "' style='height:250px'></canvas>";
+        html += "</div>";
+        html += "</div>";
+        html += "<!-- /.box-body -->";
+        html += "</div>";
+
+        return html;
+    }
+
+    function FilterGroup(filtered, variableType, request) {
+        var temp = [];
+        var counter = 0;
+
+        for (var i = 0 ; i < filtered.length ; i++) {
+            if (read_prop(filtered[i], variableType) == request)
+                temp.push(filtered[i]);
+        }
+        return temp;
+    }
+
+    function countData(filtered, variableType, request) {
+        var count = 0;
+        for (var i = 0 ; i < filtered.length ; i++) {
+            if (read_prop(filtered[i], variableType) == request)
+                count++;
+        }
+        return count;
+    }
+
+    function avgData(filtered, variableType, request, valueToAvg)
+    {
+        var sum = 0;
+        for (var i = 0 ; i < filtered.length ; i++)
+        {
+            if (read_prop(filtered[i], variableType) == request)
+                sum += read_prop(filtered[i], valueToAvg);
+        }
+        return sum/filtered.length;
+    }
+
+    function httpRequest(methode, theUrl)
+    {
+        var xmlHttp = new XMLHttpRequest();
+        xmlHttp.open(methode, theUrl, false); // false for synchronous request
+        xmlHttp.send(null);
+        return xmlHttp.responseText;
+    }
+
+    function read_prop(obj, prop) {
+        return obj[prop];
+    }
+
+    function  clearGraphCanvas(elementID)
+    {
+        document.getElementById(elementID).innerHTML = "";
+        document.getElementById(elementID).className = "";
+    }
+
+    function drawBarGraph (elementID, graphID, title, type, possibleValues, labelName) {
+        document.getElementById(elementID).innerHTML = AddGraphHtml(title, graphID,"primary");
+        document.getElementById(elementID).className = "col col-md-4";
+        var dataset = [];
+        var count = 0;
+        for (var i = 0 ; i < possibleValues.length ; i++) {
+            dataset.push(countData(filteredData, type, possibleValues[i]));
+        }
+
+        var barChartCanvas = $("#" + graphID).get(0).getContext("2d");
+        var barChart = new Chart(barChartCanvas);
+        var barChartData = {
+            labels: labelName,
+            datasets: [
+              {
+                  label: labelName,
+                  fillColor: "rgba(60,141,188,0.9)",
+                  strokeColor: "rgba(60,141,188,0.8)",
+                  pointColor: "#3b8bba",
+                  pointStrokeColor: "rgba(60,141,188,1)",
+                  pointHighlightFill: "#fff",
+                  pointHighlightStroke: "rgba(60,141,188,1)",
+                  data: dataset
+              }
+            ]
+        };
+        barChartOptions.datasetFill = false;
+        barChart.Bar(barChartData, barChartOptions);
+    }
+
+    function drawRiskBarGraph(elementID, graphID, title, type, possibleValues, valueToAvg, labelName)
+    {
+        document.getElementById(elementID).innerHTML = AddGraphHtml(title, graphID,"danger");
+        document.getElementById(elementID).className = "col col-md-4";
+        var dataset = [];
+        var count = 0;
+        for (var i = 0 ; i < possibleValues.length ; i++) {
+            dataset.push(avgData(filteredData, type, possibleValues[i], valueToAvg));
+        }
+        
+        var barChartCanvas = $("#" + graphID).get(0).getContext("2d");
+        var barChart = new Chart(barChartCanvas);
+        var barChartData = {
+            labels: labelName,
+            datasets: [
+              {
+                  label: labelName,
+                  fillColor: "rgba(255,0,0,0.3)",
+                  strokeColor: "rgba(255,0,0,0.3)",
+                  pointColor: "#3b8bba",
+                  pointStrokeColor: "rgba(255,0,0,0.3)",
+                  pointHighlightFill: "#fff",
+                  pointHighlightStroke: "rgba(255,0,0,0.3)",
+                  data: dataset
+              }
+            ]
+        };
+        barChartOptions.datasetFill = false;
+        barChart.Bar(barChartData, barChartOptions);
+    }
+
+
 }]);
