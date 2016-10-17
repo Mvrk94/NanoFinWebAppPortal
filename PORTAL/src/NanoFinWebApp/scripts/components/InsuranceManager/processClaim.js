f52b8c81327e6cf6cf2936157e4485a510fd03a1
margauxfourie <margauxfourie@gmail.com>
2016-10-17 03:28:23 +0200

    claim form download complete with submission of the processing of a claim

--- PORTAL/src/NanoFinWebApp/scripts/components/InsuranceManager/processClaim.js
+++ PORTAL/src/NanoFinWebApp/scripts/components/InsuranceManager/processClaim.js
@@ -1,7 +1,20 @@
 ﻿
 angular.module('myApp')
-    
+    .directive('fileInput', ['$parse', function ($parse) {
+
+        return {
+            restrict: 'A',
+            link: function (scope, element, attrs) {
+                element.bind('change', function () {//on change event bind this element to the model
+                    $parse(attrs.fileInput)
+                    .assign(scope, element[0].files);
+                    scope.$apply();
+                });
+            }
+        };
+    }])
     .controller('processClaimsCtrl', ['$scope', '$http', '$compile', '$window', '$log', '$routeParams', function ($scope, $http, $compile, $window, $log, $routeParams) {
+        $scope.apiBaseUrl = 'http://nanofinapifinal.azurewebsites.net';
 
         var CID = location.search.split('cid=')[1];
         $scope.claimID = CID;
@@ -19,10 +32,12 @@ angular.module('myApp')
             ActiveProductItems_ID: 0,
             capturedClaimFormDataJson: "string",
             claimDate: "2016-10-15T18:07:24.507Z",
-            claimStatus: "string",
-            claimPaymentFinalised: "string",
+            claimStatus: "null",
+            claimPaymentFinalised: "false",
            Consumer_ID: 0
         };
+
+        $scope.uploading = false;
 
         var getSingleClaimToBeProcessed = function () {
             $http({
@@ -44,7 +59,7 @@ angular.module('myApp')
                   $scope.productName = vClaimToBeProcessed.productName;
                   $scope.claimStatus = vClaimToBeProcessed.claimStatus;
                   $scope.claimPaymentFinalised= vClaimToBeProcessed.claimPaymentFinalised;
-
+                  $scope.consumerContactNumber = vClaimToBeProcessed.contactNumber;
 
 
                   $log.info(response);
@@ -99,6 +114,8 @@ angular.module('myApp')
         };
         getClaimDocsPath();
 
+
+
         $scope.showDocPreview = function (filename) {
             $scope.currentDocPath = $scope.claimDocsLongPath +'/'+ filename;
             $scope.nothingsBeenSelected = false;
@@ -106,17 +123,195 @@ angular.module('myApp')
             $scope.showDoc = true;
         };
 
-        $scope.uploadFile = function ()
-        {
-            //upload the file
+        var fd = new FormData();
+        $scope.upload = function (files) {
+            $scope.uploading = true;
+            
+
+            console.log(files);
+
+            if(files.length===0)
+            {
+                alert("No files selected!");
+                $scope.uploading = false;
+                return;
+            }
+            //check file size of all files //and extensions
+            for (var i = 0; i < files.length; i++) {
+                var fsize = files.item(i).size;//size of file
+                console.log('file size:' + fsize);
+
+                if ((fsize / 1024) >= 2048) {
+                    $scope.showMaxFileSizeAlert();
+                    $scope.uploading = false;
+                    return;
+                }
+
+                var fileExtension = $scope.getFileExt(files.item(i).name);
+
+                if(fileExtension ==='exe')
+                {
+                    alert("Don't allow this file type!");
+                    $scope.uploading = false;
+                    return;
+                }
+            }
+
+            angular.forEach(files, function (value, key) {
+                fd.append(key, value);
+            });
+            $http.post($scope.apiBaseUrl + '/api/FileUpload/uploadToNewDirectory?strDirectory=claims/' + $scope.productName + "/" + $scope.activeProdID, fd,
+                {
+                    transformRequest: angular.identity,
+                    headers: {
+                        'Content-Type': undefined
+                    }
+                }).success(function (d) {
+                    $scope.uploading = false;
+                    alert("Files uploaded successfully!");
+                    console.log(d);
+                }).error(function () {
+                    $scope.uploading = false;
+                    alert("Failed!");
+                });
+
         };
+
+        $scope.showMaxFileSizeAlert = function () {
+            alert("The size of one or more files is more than 2 MiB, please try again");
+
+        };
+
+        $scope.getFileExt = function (string) {
+            var array = string.split('.');
+            return array[1];
+        };
+
+        //$(document).ready(function () {
+        //    $("#myBtn").click(function () {
+        //        $("#myModal").modal();
+        //    });
+        //});
+       
 
         $scope.submitClaimProcess = function ()
         {
+            $scope.submitLoading = true;
+
+            $http({
+                method: 'PUT',
+                url: 'http://nanofinapifinal.azurewebsites.net/api/Claim/updateClaimStatus?ClaimID=' + $scope.claimID + '&status=' + $scope.claimEntity.claimStatus
+            }).then(function (response)
+            {
+
+                $log.info(response);
+            }, function (reason) {
+
+                $log.info(reason);
+            });
+
+
+            $http({
+                method: 'PUT',
+                url: 'http://nanofinapifinal.azurewebsites.net/api/Claim/updateClaimPaymentStatus?ClaimID=' + $scope.claimID + '&paymentStatus=' + $scope.claimEntity.claimPaymentFinalised
+            }).then(function (response) {
+
+                $log.info(response);
+
+
+            }, function (reason) {
+
+                $log.info(reason);
+            });
+
+
+
+
+            if($scope.claimEntity.claimStatus === "Rejected")
+            {
+                $scope.rejected = true;
+                $scope.modalHeader = "Claim Rejected";
+                $scope.modalMessage = "A notification message will be sent to the user";
+                $("#myModal").modal();
+
+                // send sms to user to notify of rejected claim
+                $http({
+                    method: 'POST',
+                    url: 'http://nanofinapifinal.azurewebsites.net/api/Notification/SendSMS?toPhoneNum=' + $scope.consumerContactNumber + '&message=' + "Your claim for: " + $scope.productName +" reference number: "+ $scope.claimID +$scope.activeProdID + " has been rejected, 2Help1 will contact you shortly. "
+                })
+                .then(function (response) {
+                    console.log("sent sms to: " + $scope.consumerContactNumber + "with message: " + message);
+                    $log.info(response);
+                }, function (reason) {
+                    console.log("failed to send sms");
+                    $log.info(reason);
+                });                     
+            }
+            if ($scope.claimEntity.claimStatus === "Accepted")
+            {
+                $scope.rejected = false;
+                $scope.modalHeader = "Claim Accepted";
+                $scope.modalMessage = "This claim will be marked as accepted and the supplied proof documentation will be made available to the user.";
+                $("#myModal").modal();
+
+            }
+            //if ($scope.claimEntity.claimStatus === "Accepted" && $scope.claimEntity.claimPaymentFinalised === false) {
+            //    $scope.rejected = false;
+            //    $scope.modalHeader = "Claim Accepted but waiting for finalising of payment";
+            //    $scope.modalMessage = "This claim will be marked as accepted, but remain on the list of claims that need further processing.";
+            //    $("#myModal").modal();
+
+            //}
+
+            if ($scope.claimEntity.claimStatus === "In Progress") {
+                $scope.rejected = false;
+                $scope.modalHeader = "Claim still In Progress";
+                $scope.modalMessage = "This claim will remain on the list of claims that need further processing.";
+                $("#myModal").modal();
+
+            }
+
+            
+
+
+          
+        };
+
+        $scope.isClaimStatusRejectedOrInProg = function ()
+        {
+            if ($scope.claimEntity.claimStatus === "Rejected" || $scope.claimEntity.claimStatus === "In Progress" || $scope.claimEntity.claimStatus==="null") {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        };
+
+        $scope.modalDismissed = function ()
+        {
             //write to db the claimStatus and Payment Finalized
-            //if rejected: display modal with user's contact information
+
+         
+
+            window.location = "/viewClaims";
 
         };
 
+        $scope.genPDF = function ()
+        {
+            html2canvas(document.getElementById('claimfrm'), {
+                onrendered: function (canvas) {
+                    var data = canvas.toDataURL();
+                    var docDefinition = {
+                        content: [{
+                            image: data,
+                            width: 500,
+                        }]
+                    };
+                    pdfMake.createPdf(docDefinition).download("#"+$scope.claimID+$scope.activeProdID +".pdf");
+                }
+            });           
+        };
 
     }]);
